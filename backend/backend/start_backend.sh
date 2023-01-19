522426d94742c51321e20ec308d74dc07bf91af0
Wegner <wegnerp@BN-M-3018921dzne.ds>
2023-01-19 11:08:51 +0100

    added stuff

--- backend/backend/start_backend.sh
+++ backend/backend/start_backend.sh
@@ -2,7 +2,8 @@ export SCA_VIEW_EAV_PATH=../data/virtual_cohort.pkl
 export SCA_VIEW_DMODEL_PATH=../dmodels/Datamodel_reduced.xlsx
 PORT=8001
 export SCA_VIEW_PATH=$PORT
-SCA_VIEW_PROD_URL=http://idsn.dzne.de/clinical-backend/
+export SCA_VIEW_BASE_PATH=clinical-backend/
+export SCA_VIEW_PROD_URL=http://idsn.dzne.de/clinical-backend/
 python manage.py runserver 0.0.0.0:$PORT
 
 
--- backend/backend/viewer/settings.py
+++ backend/backend/viewer/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-BASEPATH="clinical-backend/"
+BASEPATH = os.environ.get("SCA_VIEW_BASE_PATH")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
