21072275f26dc10c6918fe1282e53489eea16cf7
Philipp <philipp.wegner@scai.fraunhofer.de>
2023-01-27 11:36:06 +0100

    added stuff

--- backend/backend/start_backend.sh
+++ backend/backend/start_backend.sh
@@ -1,9 +1,14 @@
-export SCA_VIEW_EAV_PATH=../data/eav.pkl
+
+
+
+
+
+export SCA_VIEW_EAV_PATH=../data/virtual_cohort.pkl
 export SCA_VIEW_DMODEL_PATH=../dmodels/Datamodel_reduced.xlsx
-PORT=8000
+PORT=8001
 export SCA_VIEW_PATH=$PORT
-export SCA_VIEW_DEBUG=TRUE
-export SCA_VIEW_BASE_PATH=clinical-backend/
+export SCA_VIEW_DEBUG=FALSE
+export SCA_VIEW_BASE_PATH=clinical-backend-syn/
 export SCA_VIEW_PROD_URL=https://idsn.dzne.de/$SCA_VIEW_BASE_PATH
 python manage.py runserver 0.0.0.0:$PORT
 
