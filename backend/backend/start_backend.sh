33871c26dd6be39d4ae25382010a730cf5a49ee4
Wegner <wegnerp@BN-M-3018921dzne.ds>
2023-02-13 09:44:42 +0100

    added stuff

--- backend/backend/start_backend.sh
+++ backend/backend/start_backend.sh
@@ -8,7 +8,7 @@ export SCA_VIEW_DMODEL_PATH=../dmodels/Datamodel_reduced.xlsx
 PORT=8001
 export SCA_VIEW_PATH=$PORT
 export SCA_VIEW_DEBUG=FALSE
-export SCA_VIEW_BASE_PATH=clinical-backend-syn/
+export SCA_VIEW_BASE_PATH=clinical-backend/
 export SCA_VIEW_PROD_URL=https://idsn.dzne.de/$SCA_VIEW_BASE_PATH
 python manage.py runserver 0.0.0.0:$PORT
 
---
+++ backend/scripts/insert_inas_count.py
@@ -0,0 +1,8 @@
+# %%
+import pandas as pd
+
+eav = pd.read_pickle("../data/eav.pkl")
+
+orig_data = pd.read_csv("../original_daten/")
+
+# %%
